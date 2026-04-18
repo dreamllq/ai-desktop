df430dcd548b03ce41fcbc20a9bd97ad28676632
lvliqi <lvliqi_d@163.com>
2026-04-18 18:00:25 +0800

    fix(settings): remove sidebar from Settings window

--- src/renderer/src/App.vue
+++ src/renderer/src/App.vue
@@ -1,13 +1,19 @@
 <script setup lang="ts">
+import { computed } from 'vue'
+import { useRoute } from 'vue-router'
 import { useAppStore } from '@renderer/stores/app'
 import AppLayout from '@renderer/components/layout/AppLayout.vue'
 
+const route = useRoute()
 const store = useAppStore()
 store.initialize()
+
+const showSidebar = computed(() => !route.path.startsWith('/settings'))
 </script>
 
 <template>
-  <AppLayout>
+  <AppLayout v-if="showSidebar">
     <RouterView />
   </AppLayout>
+  <RouterView v-else />
 </template>
